5be37a6a4ab6ebd607fbf5a11f881fb0f9ffefd9
ValentinBuletin <braneanu_valentin@yahoo.com>
2024-05-02 13:37:27 +0300

    DB and Index refactoring

--- index.php
+++ index.php
@@ -1,18 +1,84 @@
 <?php
 
-define('ROOT', __DIR__);
-require_once(ROOT . '/utils/NewsManager.php');
-require_once(ROOT . '/utils/CommentManager.php');
+namespace Database;
 
-foreach (NewsManager::getInstance()->listNews() as $news) {
-	echo("############ NEWS " . $news->getTitle() . " ############\n");
-	echo($news->getBody() . "\n");
-	foreach (CommentManager::getInstance()->listComments() as $comment) {
-		if ($comment->getNewsId() == $news->getId()) {
-			echo("Comment " . $comment->getId() . " : " . $comment->getBody() . "\n");
-		}
-	}
+use Database\utils\NewsManager;
+use Database\utils\CommentManager;
+
+// Global variable
+const ROOT = __DIR__; // Assigning to ROOT the absolute path of the project
+
+// Validating the required files are present
+require_once(ROOT . "/utils/NewsManager.php");
+require_once(ROOT . "/utils/CommentManager.php");
+
+/**
+ * Displays all the News (Title, Body) and the Comments (ID, Body) associated to it
+ * @return void
+ */
+function displayDataBaseData(): void
+{
+
+    // Iterate through all the returned News entities
+    foreach (NewsManager::getInstance()->listNews() as $news) {
+        // Display the Title and the Body of the News entity
+        echo("############ NEWS " . $news->getTitle() . " ############\n");
+        echo($news->getBody() . "\n");
+        // Get all the comments that are associated to the News ID and iterate through them
+        foreach (CommentManager::getInstance()->listComments($news->getId()) as $comment) {
+            echo("Comment " . $comment->getId() . " : " . $comment->getBody() . "\n");
+        }
+    }
 }
 
-$commentManager = CommentManager::getInstance();
-$c = $commentManager->listComments();
+
+/**
+ * Function used to test all the methods from inside CommentManager
+ * @return void
+ */
+function testCommentManagerFunctions(): void
+{
+    print ("\n Comment Manager Functions\n");
+
+    $cm = CommentManager::getInstance();
+
+    $tmp_comments = $cm->listComments(1);
+    foreach ($tmp_comments as $comment) {
+        print $comment->getid() . "\t";
+        print $comment->getbody() . "\t";
+        print $comment->getCreatedAt()->format('Y-m-d H:i:s') . "\t";
+        print $comment->getNewsId() . "\n";
+    }
+
+    $cm->addComment("This is a new comment", "3");
+
+    $cm->deleteComment(60);
+
+}
+
+/**
+ * Function used to test all the methods from inside CommentManager
+ * @return void
+ */
+function testNewsManagerFunctions(): void
+{
+    print ("\n News Manager Functions\n");
+
+    $nm = NewsManager::getInstance();
+
+    $tmp_comments = $nm->listNews();
+    foreach ($tmp_comments as $comment) {
+        print $comment->getid() . "\t";
+        print $comment->gettitle() . "\t";
+        print $comment->getbody() . "\t";
+        print $comment->getCreatedAt()->format('Y-m-d H:i:s') . "\n";
+    }
+
+    $nm->addNews("Great Title", "This is the body");
+
+    $nm->deleteNews(16);
+}
+
+displayDataBaseData();
+//testCommentManagerFunctions();
+//testNewsManagerFunctions();
--- utils/DB.php
+++ utils/DB.php
@@ -1,42 +1,104 @@
 <?php
 
+namespace Database\utils;
+
+use Exception;
+use PDO;
+use PDOException;
+
 class DB
 {
-	private $pdo;
+    // Private Variables
+    public PDO $pdo;
+    private static mixed $instance = null;
 
-	private static $instance = null;
+    /**
+     * Constructor reads Database credentials from file and connects to it using pdo
+     * @param string $file
+     * @throws Exception
+     */
+    private final function __construct(string $file = "settings.ini")
+    {
+        // Using a separate file where we keep the DB parameters for improved security
+        try {
+            // If the file exists
+            if (file_exists($file)) {
+                // We parse through it
+                $settings = parse_ini_file($file);
+            } else {
+                // If the file does not exist then throw an Exception
+                throw new Exception("File $file does not exist!");
+            }
+        } catch (Exception $e) {
+            throw new Exception("Unable to open $file\t" . $e->getMessage());
+        }
 
-	private function __construct()
-	{
-		$dsn = 'mysql:dbname=phptest;host=127.0.0.1';
-		$user = 'root';
-		$password = 'pass';
+        try {
+            $dsn = "{$settings["driver"]}:dbname={$settings["dbname"]};host={$settings["host"]};port={$settings["port"]}";
+            // Make the DB connection
+            $this->pdo = new PDO($dsn, $settings["user"], $settings["password"]);
 
-		$this->pdo = new \PDO($dsn, $user, $password);
-	}
+            print("DB started successfully!\n");
+        } catch (PDOException $e) {
+            throw new Exception("Error when starting DB!:\t" . $e->getMessage());
+        }
+    }
 
-	public static function getInstance()
-	{
-		if (null === self::$instance) {
-			$c = __CLASS__;
-			self::$instance = new $c;
-		}
-		return self::$instance;
-	}
+    /**
+     * Singleton instance
+     * @return mixed|null
+     */
+    public static function getInstance(): mixed
+    {
+        if (null === self::$instance) {
+            $c = __CLASS__;
+            self::$instance = new $c;
+        }
+        return self::$instance;
+    }
 
-	public function select($sql)
-	{
-		$sth = $this->pdo->query($sql);
-		return $sth->fetchAll();
-	}
+    /**
+     * Executes the given query on the DB and returns all the results/rows
+     * @param string $sql
+     * @return false|array
+     */
+    public function select(string $sql): false|array
+    {
+        $sth = $this->pdo->query($sql);
+        return $sth->fetchAll();
+    }
 
-	public function exec($sql)
-	{
-		return $this->pdo->exec($sql);
-	}
+    /**
+     * Prepares and executes the query on the DB
+     * @param string $sql
+     * @param array $exec_arg
+     * @param bool $delete
+     * @return bool
+     */
+    public function exec(string $sql, array $exec_arg, bool $delete = false): bool
+    {
+        // Prepare the SQL query
+        $prepared_sql = $this->pdo->prepare($sql);
+        // Execute the SQL query
+        $execute_response = $prepared_sql->execute($exec_arg);
 
-	public function lastInsertId()
-	{
-		return $this->pdo->lastInsertId();
-	}
+        // If the functions is used to execute a DELETE query
+        if ($delete) {
+            // Check the rowCount of the DB to see if the row was deleted or not
+            // as it is possible that the provided ID does not exist in the DB
+            return $prepared_sql->rowCount();
+        } else {
+            return $execute_response;
+        }
+    }
+
+    /**
+     * Returns the last inserted ID in the Database
+     * @return false|string
+     */
+    public function getLastInsertId(): false|string
+    {
+        return $this->pdo->lastInsertId();
+    }
 }
+
